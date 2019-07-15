2ba9303acc8fb3d07a13e60004d56ea87bdcc985
coding_python7 <slssg025@gmail.com>
2019-07-15 16:04:14 -0700

    finds average of numbers

---
+++ TurtleTest.py
@@ -0,0 +1,18 @@
+import turtle
+import random as rand
+t = turtle.Pen()
+wn = turtle.Screen()
+
+
+while true:
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+
+
+wn.exitonclick()
