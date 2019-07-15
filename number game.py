ade0db142ba94e0712867a1b12f83a9e87055e10
coding_python7 <slssg025@gmail.com>
2019-07-15 15:04:06 -0700

    I printed hello

--- number game.py
+++ number game.py
@@ -1,10 +1,16 @@
-def main():
-    x=10
-    user = input('Please Input Number')
+import random as rand
+
+x = rand.randrange(1,100)
+UserCorrect = False
+
+while(UserCorrect == False):
+    print('Please input a number')
+    user = input()
     user_int = int(user)
 
     if (user_int == x):
         print('Equal!')
+        quit()
 
     if (user_int > x):
         print("Greater Than")
@@ -13,5 +19,5 @@ def main():
         print('Less than')
 
 
-if(__name__== '__main__'):
-    main()
+
+
